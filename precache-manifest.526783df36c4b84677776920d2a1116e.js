80adf9c8bfdf0e442254edf8fc67c1ca6c7a6a22
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2020-12-29 18:46:27 +0530

    Updates

---
+++ precache-manifest.526783df36c4b84677776920d2a1116e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8656399e554ce638cbd25e8b78e5f6e4",
+    "url": "/origin-to-server-socket-image/index.html"
+  },
+  {
+    "revision": "515129a2d2f5ff34fbe9",
+    "url": "/origin-to-server-socket-image/static/css/main.6f518fba.chunk.css"
+  },
+  {
+    "revision": "13d8c752cfbaf0c11141",
+    "url": "/origin-to-server-socket-image/static/js/2.e2375e9c.chunk.js"
+  },
+  {
+    "revision": "7e16ec243c4ff140cc8ec9d5f98abcad",
+    "url": "/origin-to-server-socket-image/static/js/2.e2375e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "515129a2d2f5ff34fbe9",
+    "url": "/origin-to-server-socket-image/static/js/main.b56b812e.chunk.js"
+  },
+  {
+    "revision": "349e7e9573a03b0ea323",
+    "url": "/origin-to-server-socket-image/static/js/runtime-main.a3eb63bc.js"
+  }
+]);
